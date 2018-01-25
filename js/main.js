2f7b90199c91bc83d30c40481301bc0bd81895df
Nikita Kapshtyk <viks2332@gmail.com>
2018-01-25 22:11:53 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// $('.about-slider').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 4,
+//   adaptiveHeight: true
+// });
+
+// $('.about-slider').slick({
+//   slidesToShow: 4,
+//   slidesToScroll: 1
+// });
+
+
+
+$('.about-slider').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true,
+ //  responsive: [
+ //    {
+ //      breakpoint: 992,
+ //      settings: {
+ //        slidesToShow: 3,
+ //        slidesToScroll: 3,
+ //        infinite: true,
+ //        dots: true,
+ //        centerMode: true,
+ //  		variableWidth: true
+ //      }
+   
+	// }
+ //   ] 
+});
+
+// $('.about-slider').slick('slickCurrentSlide').;
+
+
+
+
+
+// $(function(){
+//   $('img').socialpic();
+// });
